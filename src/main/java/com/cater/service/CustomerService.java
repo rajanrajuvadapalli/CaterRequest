f372b99479908c023e4902ae1521543eaa4df166
Hari Samala <hari.samala@vsp.com>
2014-12-09 15:22:25 -0800

    Added functionality to create events.

--- src/main/java/com/cater/service/CustomerService.java
+++ src/main/java/com/cater/service/CustomerService.java
@@ -6,12 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.cater.dao.CustomerDAO;
+import com.cater.dao.EventDAO;
 import com.cater.model.Customer;
+import com.cater.model.Event;
 
 @Component
 public class CustomerService {
 	@Autowired
 	private CustomerDAO customerDAO;
+	@Autowired
+	private EventDAO eventDAO;
 
 	public List<Customer> fetchAllCustomers() {
 		return customerDAO.fetchAllCustomers();
@@ -19,5 +23,9 @@ public class CustomerService {
 
 	public Customer fetchCustomerWithLoginId(int loginID) {
 		return customerDAO.findByLoginID(loginID);
+	}
+
+	public boolean saveEvent(Event e) {
+		return eventDAO.save(e);
 	}
 }
