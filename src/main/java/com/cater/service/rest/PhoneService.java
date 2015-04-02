bdebdde3b882bc3562354eeae595fa25d0b51788
Hari Samala <hari2139@gmail.com>
2015-04-02 07:57:43 -0700

    Added REST-Easy jars

---
+++ src/main/java/com/cater/service/rest/PhoneService.java
@@ -0,0 +1,12 @@
+package com.cater.service.rest;
+
+import javax.ws.rs.Path;
+
+/**
+ * Description: 
+ * Created: Apr 2, 2015
+ * @author Hari 
+ */
+@Path("/phone")
+public class PhoneService {
+}
